f05cb3d4d6cd06daeff9540e3a6e57b121619794
Christian Zagrodnick <cz@flyingcircus.io>
2008-02-27 11:48:33 +0000

    preparing for 0.1.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gocept.sftpcopy',
-    version = "0.1.3dev",
+    version = "0.1.4dev",
     author = "Christian Zagrodnick",
     author_email = "cz@gocept.com",
     description = "Copying files save to another machine",
