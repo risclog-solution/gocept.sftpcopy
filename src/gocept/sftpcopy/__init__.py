608fc9e2914c9b85064b7cbcadaf2fc063c05c49
Wolfgang Schnerring <ws@gocept.com>
2012-01-25 14:31:07 +0100

    Remove zope.deferredimport

--- src/gocept/sftpcopy/__init__.py
+++ src/gocept/sftpcopy/__init__.py
@@ -1,9 +1,4 @@
 # Copyright (c) 2007 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
 
-import zope.deferredimport
-
-zope.deferredimport.define(
-    SFTPCopy = 'gocept.sftpcopy.sftpcopy:SFTPCopy'
-    )
+from gocept.sftpcopy.sftpcopy import SFTPCopy
