ad32995d054a953f04fe7d4763a41592d2b3e679
Wolfgang Schnerring <wosc@wosc.de>
2014-02-20 11:45:48 +0100

    Back to development: 0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os.path
 
 setup(
     name='gocept.sftpcopy',
-    version='0.3.0',
+    version='0.3.1.dev0',
     author="Christian Zagrodnick <cz at gocept dot com>",
     author_email="cz@gocept.com",
     description="Upload/download files via SFTP to a  maildir structure",
